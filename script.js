2b94b9c221e9e7745c5513ff786aecf8020cfe67
Henriquetied <hgoulart659@gmail.com>
2021-05-01 12:31:45 -0300

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const firePixelsArray = []
+const fireWidth = 2
+const fireHeight = 3
+
+function start() {
+    createFireDataStructure()
+    console.info(firePixelsArray)
+}
+
+
+function createFireDataStructure() {
+    const numberOfPixels = fireWidth * fireHeight
+
+    for(let i = 0; i < numberOfPixels; i++) {
+        firePixelsArray[i] = 0
+    }
+}
+
+function calculateFirePropagation() {
+
+}
+
+function RenderFire() {
+
+}
+
+start()
